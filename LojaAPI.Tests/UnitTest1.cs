77ea1a146d368c557e9f86c577b9dbf8c2bfa2e6
ediele <ediele.ediele@hotmail.com>
2017-02-15 21:11:15 -0200

    Inclusão do projeto inicial

---
+++ LojaAPI.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LojaAPI.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ LojaAPI/Entidade/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LojaAPI.Entidade
+{
+    public class Categoria
+    {
+        public int IdCategoria { get; set; }
+        string Nome { get; set; }
+        string propriedade { get; set; }
+        string propriedade2 { get; set; }        
+    }
+}
